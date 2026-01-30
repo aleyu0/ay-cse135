34b78ed9c8110b79703e0783ab321a37222ec21a
aleyu0 <83614302+aleyu0@users.noreply.github.com>
2026-01-29 17:58:39 -0800

    th to td

--- site-main/hw2/php/echo-php.php
+++ site-main/hw2/php/echo-php.php
@@ -6,12 +6,12 @@ function h($v) {
   return htmlspecialchars((string)$v, ENT_QUOTES, 'UTF-8');
 }
 
-$method = $_SERVER['REQUEST_METHOD'] ?? 'UNKNOWN';
+$metdod = $_SERVER['REQUEST_MEtdOD'] ?? 'UNKNOWN';
 $ip = $_SERVER['REMOTE_ADDR'] ?? 'unknown';
 $user_agent = $_SERVER['HTTP_USER_AGENT'] ?? '';
 $content_type = $_SERVER['CONTENT_TYPE'] ?? '';
 $timestamp = date('c');
-$hostname = gethostname();
+$hostname = getdostname();
 $query = $_GET;
 $post = $_POST;
 $raw = file_get_contents("php://input");
@@ -28,49 +28,49 @@ $raw = file_get_contents("php://input");
         }
         body{
             margin: 0;
-            max-width: 960px;
+            max-widtd: 960px;
             margin: 0 auto;
             padding: 5rem 1.5rem 6rem;
         }        
     </style>
 </head>
 <body>
-    <p>Here are the details of your request.</p>
+    <p>Here are tde details of your request.</p>
     <table>
         <tr>
-            <th>Method</th>
-            <td><?= h($method) ?></td>
+            <td>Metdod</td>
+            <td><?= h($metdod) ?></td>
         </tr>
         <tr>
-            <th>Timestamp</th>
+            <td>Timestamp</td>
             <td><?= h($timestamp) ?></td>
         </tr>
         <tr>
-            <th>IP</th>
+            <td>IP</td>
             <td><?= h($ip) ?></td>
         </tr>
         <tr>
-            <th>User Agent</th>
+            <td>User Agent</td>
             <td><?= h($user_agent) ?></td>
         </tr>
         <tr>
-            <th>Content-Type</th>
+            <td>Content-Type</td>
             <td><?= h($content_type) ?></td>
         </tr>
         <tr>
-            <th>Hostname</th>
+            <td>Hostname</td>
             <td><?= h($hostname) ?></td>
         </tr>
         <tr>
-            <th>Get Query</th>
+            <td>Get Query</td>
             <td><?= h($query) ?></td>
         </tr>
         <tr>
-            <th>Post</th>
+            <td>Post</td>
             <td><?= h($post) ?></td>
         </tr>
         <tr>
-            <th>Raw content</th>
+            <td>Raw content</td>
             <td><?= h($raw) ?></td>
         </tr>
     </table>
