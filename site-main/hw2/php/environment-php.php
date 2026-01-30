14e1f0e3b94afe26cbc4159e76a4540e2180cbd2
aleyu0 <83614302+aleyu0@users.noreply.github.com>
2026-01-29 16:39:31 -0800

    obscure sensitive info

--- site-main/hw2/php/environment-php.php
+++ site-main/hw2/php/environment-php.php
@@ -1,4 +1,6 @@
 <?php
+$skip = ['AUTH_TYPE','PHP_AUTH_USER', 'PHP_AUTH_PW', 'HTTP_AUTHORIZATION'];
+
 header("Cache-Control: no-cache");
 header("Content-Type: text/html; charset=UTF-8");
 
@@ -10,8 +12,10 @@ echo "<h1>Environment Variables (PHP)</h1><hr/>";
 echo "<h2>\$_SERVER</h2><pre>";
 ksort($_SERVER);
 foreach ($_SERVER as $k => $v) {
+  if (in_array($k, $skip, true)) continue;
   echo h($k) . " = " . h($v) . "\n";
 }
 echo "</pre>";
 
 echo "</body></html>";
+>
