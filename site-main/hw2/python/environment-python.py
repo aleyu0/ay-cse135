af3a8af2469481603a484d5007a7bc79643b69c3
aleyu0 <83614302+aleyu0@users.noreply.github.com>
2026-01-29 16:47:43 -0800

    py env

---
+++ site-main/hw2/python/environment-python.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import os
+
+print("Cache-Control: no-cache")
+print("Content-Type: text/html; charset=UTF-8\n")
+
+print("<!doctype html><html><head><title>Environment (Python)</title></head><body>")
+print("<h1>Environment Variables (Python)</h1><hr/>")
+print("<pre>")
+for k in sorted(os.environ.keys()):
+    v = os.environ.get(k, "")
+    # basic HTML escaping
+    v = (v.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;"))
+    print(f"{k} = {v}")
+print("</pre></body></html>")
