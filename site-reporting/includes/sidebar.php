c126eaacaf2cca9a89e08ec0146906e7cfdd4beb
aleyu0 <83614302+aleyu0@users.noreply.github.com>
2026-03-13 19:15:38 -0700

    add to nav

--- site-reporting/includes/sidebar.php
+++ site-reporting/includes/sidebar.php
@@ -42,6 +42,13 @@ $userRole = $_SESSION['role'] ?? ($currentUser['role'] ?? 'viewer');
     </a>
     <?php endif; ?>
 
+    <?php if (has_permission('view-reports')): ?>
+    <a href="reports.php" <?= $currentPage === 'reports.php' ? 'class="active"' : '' ?>>
+      <img src="assets/icons/report.svg" alt="" width="16" height="16" class="nav-icon" />
+      Reports
+    </a>
+    <?php endif; ?>
+
     <a href="admin.php" <?= $currentPage === 'admin.php' ? 'class="active"' : '' ?>>
       <img src="assets/icons/users.svg" alt="" width="16" height="16" class="nav-icon" />
       <?= has_permission('view-users') ? 'Users' : 'My Account' ?>
